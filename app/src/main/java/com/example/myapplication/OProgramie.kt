c3df20d33977dc5d33dffea042236c71e55ba318
MT12871 <147538244+MT12871@users.noreply.github.com>
2023-12-02 17:36:51 +0100

    Dodanie nowych ekranów w menu startu

--- app/src/main/java/com/example/myapplication/OProgramie.kt
+++ app/src/main/java/com/example/myapplication/OProgramie.kt
@@ -1,4 +1,21 @@
 package com.example.myapplication
 
-class OProgramie {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class OProgramie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oprogramie)
+
+        val przyciskwroc2 = findViewById<ImageButton>(R.id.PrzyciskWroc2)
+
+        przyciskwroc2.setOnClickListener {
+            val intent = Intent(this, EkranStartowy::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
